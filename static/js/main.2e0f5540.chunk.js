78d5153bd1fb6185df3563df3a3e770d55301ae1
ryo-i <iidazz0109@gmail.com>
2021-02-01 07:19:40 +0900

    Updates

---
+++ static/js/main.2e0f5540.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-from-meta-lang"]=this["webpackJsonpreact-from-meta-lang"]||[]).push([[0],[function(e,t){var n=JSON.parse('{"message":{"text":"\u3053\u3093\u306b\u3061\u306f\u3001\u3075\u308d\u3093\u3068\u3048\u3093\u3069\u3002","selector":".inner__text--hello"}}'),o={text:n.message.text,selector:n.message.selector},s=o.text,a=o.selector;document.addEventListener("DOMContentLoaded",(function(){document.querySelector(a).innerHTML=s,console.log("text-> "+s)}))}],[[0,1]]]);
+//# sourceMappingURL=main.2e0f5540.chunk.js.map
