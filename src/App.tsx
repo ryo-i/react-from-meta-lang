b5806001337be58355f4843de94666397ab52857
ryo-i <iidazz0109@gmail.com>
2021-02-04 05:48:20 +0900

    helmetでheadを書き換え

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import {Helmet} from "react-helmet";
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 
+const titleText = 'Reactとメタ言語の比較';
+const descriptionText = '以前作ったメタ言語スターターキットの内容をReact環境で再現してみる';
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+        <Helmet
+            title={ titleText }
+            meta={[
+                { name: 'description', content: descriptionText }
+            ]}
+         />
+        <Header />
+        <Main />
+        <Footer />
     </div>
   );
 }
