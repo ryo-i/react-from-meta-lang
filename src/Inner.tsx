07dba8928e7ccdbee07548ad06c678f7aedabdbc
ryo-i <iidazz0109@gmail.com>
2021-02-08 04:54:45 +0900

    key名変更

--- src/Inner.tsx
+++ src/Inner.tsx
@@ -7,10 +7,10 @@ const innerJson = Data.data.inner;
 function Inner() {
   return (
     <div className="inner">
-      {innerJson.map((val) => 
-        <section key={ val.toString() }>
-          <h2 className="inner__title">{ val.title }</h2>
-          <p className="inner__text" dangerouslySetInnerHTML={{ __html: val.text }}></p>
+      {innerJson.map((innerJson, index) => 
+        <section key={ index }>
+          <h2 className="inner__title">{ innerJson.title }</h2>
+          <p className="inner__text" dangerouslySetInnerHTML={{ __html: innerJson.text }}></p>
         </section>
       )}
     </div>
